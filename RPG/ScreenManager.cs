5862e29de49be38d5d04f573779f9e07a52ab416
TanyaRan <tatyanaran@gmail.com>
2016-06-13 23:41:04 +0300

    Menu test

---
+++ RPG/ScreenManager.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RPG
+{
+    // managed our screens
+    public class ScreenManager
+    {
+        // singleton class -> only one instance
+        private static ScreenManager instance;
+        GameScreen currentScreen;
+
+        public ScreenManager()
+        {
+            Dimentions = new Vector2(640, 480);
+            currentScreen = new SplashScreen();
+        }
+
+        public Vector2 Dimentions { get; private set; }
+        public ContentManager Content { get; private set; }
+
+        public static ScreenManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ScreenManager();
+                }
+
+                return instance;
+            } // no set accessor
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            this.Content = new ContentManager(Content.ServiceProvider, "Content");
+            currentScreen.LoadContent();
+        }
+
+        // will unload all we don't needed
+        public void UnloadContent()
+        {
+            currentScreen.UnloadContent();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            currentScreen.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            currentScreen.Draw(spriteBatch);
+        }
+    }
+}
---
+++ RPG/SplashScreen.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RPG
+{
+    public class SplashScreen : GameScreen
+    {
+        Texture2D image;
+        public string Path;
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            Path = "menu";
+            image = content.Load<Texture2D>(Path);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            //base.Draw(spriteBatch);
+            spriteBatch.Draw(image, Vector2.Zero, Color.White);
+        }
+    }
+}
